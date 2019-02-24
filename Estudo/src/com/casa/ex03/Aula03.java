e2d59e1d228f27d1c9166191199279faffb83e4d
Luis Gritz <Luis Gritz@DESKTOP-4MT6SKQ>
2019-02-24 19:09:50 -0300

    Teste01

--- Estudo/src/com/casa/ex03/Aula03.java
+++ Estudo/src/com/casa/ex03/Aula03.java
@@ -7,8 +7,9 @@ public class Aula03 {
 	public static void main(String[] args) {
 
 		Caneta c1 = new Caneta("Bic", "Azul", 1.2f);
-	
+		Caneta c2 = new Caneta("Nihj", "Branca", 1.7f);
 		c1.status();
+		c2.status();
 
 	}
 
