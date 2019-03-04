951529af907f2a139ac93abd44542a3be1bd2adf
Luis-Gritz <37088336+Luis-Gritz@users.noreply.github.com>
2019-03-04 20:09:52 -0300

    Add files via upload

---
+++ Estudo/src/com/casa/ex05/Aula05.java
@@ -0,0 +1,18 @@
+package com.casa.ex05;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+		
+		ControleRemoto c1 = new ControleRemoto();
+		
+		c1.ligar();
+		c1.play();
+		c1.maisVolume();
+		c1.abrirMenu();
+		c1.ligarMudo();
+		c1.abrirMenu();
+
+	}
+
+}
